9c25190ce9607a3c120f73b3de68159fddec0de0
Rachith-M <rachithm.ad24@bmsce.ac.in>
2025-12-22 00:28:05 +0530

    Update and rename something.py to ids.py

---
+++ Python Progs/ids.py
@@ -0,0 +1,36 @@
+t= {
+    'A': ['B', 'C', 'D'],
+    'B': ['E', 'F'],
+    'C': ['G'],
+    'D': ['H', 'I'],
+    'E': [],
+    'F': [],
+    'G': [],
+    'H': [],
+    'I': []
+}
+
+
+def dfs_limited(node, goal, depth):
+    if node == goal:
+        return True
+    if depth<=0:
+        return False
+    
+    for neighbour in t.get(node,[]):
+        if dfs_limited(neighbour,goal,depth - 1):
+            return True
+    return False
+
+def ids(start,goal,max_depth):
+    for depth in range(max_depth + 1):
+        if dfs_limited(start, goal, depth):
+            print(f"Goal node:{goal} found at depth:{depth}")
+            return True
+    print(f"Goal node: {goal} not found")
+    return False
+
+start_node = 'A'
+goal_node = 'G'
+depth_limit = 4
+ids(start_node, goal_node,depth_limit)
